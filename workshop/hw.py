14c75bc3c8e9446cbb84f54e587a92786819d1c7
a920604a <a920604a@gmail.com>
2025-07-08 23:25:03 +0800

    update workshop

--- workshop/hw.py
+++ workshop/hw.py
@@ -1,5 +1,11 @@
 import requests
+from dlt.destinations import qdrant
 
+import dlt
+print(dlt.__version__)
+
+
+@dlt.resource
 def zoomcamp_data():
     docs_url = 'https://github.com/alexeygrigorev/llm-rag-workshop/raw/main/notebooks/documents.json'
     docs_response = requests.get(docs_url)
@@ -11,3 +17,19 @@ def zoomcamp_data():
         for doc in course['documents']:
             doc['course'] = course_name
             yield doc
+            
+
+
+qdrant_destination = qdrant(qd_path="db.qdrant")
+
+pipeline = dlt.pipeline(
+    pipeline_name="zoomcamp_pipeline",
+    destination=qdrant_destination,
+    dataset_name="zoomcamp_tagged_data"
+)
+
+load_info = pipeline.run(zoomcamp_data())
+
+# 印出執行記錄
+print(pipeline.last_trace)
+
